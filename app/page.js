e6946d2a53129e5f30317b32628c7f161c46309f
kevinzhxng <kzhang0826@gmail.com>
2024-08-06 14:23:19 -0400

    added vercel analysitcs

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -16,7 +17,8 @@ export default function Home() {
     setMessage("");
     setMessages((messages) => [
       ...messages,
-      { role: "user", content: message }, {role: 'assistant', content: ''},
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
     ]);
     const response = fetch("/api/chat", {
       method: "POST",
@@ -39,7 +41,10 @@ export default function Home() {
           let lastMessage = messages[messages.length - 1];
           let otherMessages = messages.slice(0, messages.length - 1);
 
-          return [...otherMessages, {...lastMessage, content: lastMessage.content + text}];
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
         });
         return reader.read().then(processText);
       });
@@ -48,64 +53,67 @@ export default function Home() {
   const [message, setMessage] = useState("");
 
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        direction={"column"}
-        width="500px"
-        height="700px"
-        border="1px solid black"
-        p={2}
-        spacing={3}
+    <>
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
       >
         <Stack
           direction={"column"}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="700px"
+          width="500px"
+          height="700px"
+          border="1px solid black"
+          p={2}
+          spacing={3}
         >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === "assistant" ? "flex-start" : "flex-end"
-              }
-            >
+          <Stack
+            direction={"column"}
+            spacing={2}
+            flexGrow={1}
+            overflow="auto"
+            maxHeight="700px"
+          >
+            {messages.map((message, index) => (
               <Box
-                bgcolor={
-                  message.role === "assistant"
-                    ? "primary.main"
-                    : "secondary.main"
+                key={index}
+                display="flex"
+                justifyContent={
+                  message.role === "assistant" ? "flex-start" : "flex-end"
                 }
-                color="white"
-                borderRadius={16}
-                p={3}
               >
-                {message.content}
+                <Box
+                  bgcolor={
+                    message.role === "assistant"
+                      ? "primary.main"
+                      : "secondary.main"
+                  }
+                  color="white"
+                  borderRadius={16}
+                  p={3}
+                >
+                  {message.content}
+                </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
+          </Stack>
+          <Stack direction={"row"} spacing={2}>
+            <TextField
+              label="Message"
+              fullWidth
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button variant="contained" onClick={sendMessage}>
+              Send
+            </Button>
+          </Stack>
         </Stack>
-        <Stack direction={"row"} spacing={2}>
-          <TextField
-            label="Message"
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <Button variant="contained" onClick={sendMessage}>
-            Send
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
+      </Box>
+      <Analytics />
+    </>
   );
 }
